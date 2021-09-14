1392f3261f5d896782cb97a6eb9f8b5c9ba65556
Alexandr <alexandryalovoy@gmail.com>
2021-09-15 00:15:03 +0300

    15/09/2021 0.15

---
+++ Chemical_base/Gas_c_b.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa on 14.09.2021.
+//
+
+#include "Gas_c_b.h"
---
+++ Chemical_base/Gas_c_b.h
@@ -0,0 +1,8 @@
+//
+// Created by alexa on 14.09.2021.
+//
+
+#ifndef GASPROPERTIES_GAS_C_B_H
+#define GASPROPERTIES_GAS_C_B_H
+
+#endif //GASPROPERTIES_GAS_C_B_H
